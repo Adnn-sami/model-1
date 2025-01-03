d0f9e1d8781e658d2ec5fd99234b4a727a568435
Adnn-sami <adnnsami857@gmail.com>
2025-01-03 20:32:57 +0600

    this is the first set of solved homeworks

---
+++ homework1.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+//this is the code file of homework of the tutorial im learning from apna college
+//this is the first edition
+int main(int argc, char const *argv[])
+
+   {
+
+//solution a
+//float a is base 
+// float b is height
+int a;
+int b;
+printf("enter length:");
+scanf("%d", &a);
+printf("enter width:");
+scanf("%d", &b);
+int c = a * b;
+printf("perimeter is %d \n" , c);
+//solution b
+int x;
+printf("write number n:");
+scanf("%d",&x);
+printf(" cube is %d",x*x*x) ;
+
+//comment has been written
+
+return 0;    
+}
---
+++ homework2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+//solution a
+//int a,b,c used for taking variables
+int a;
+printf("write 1st number:");
+scanf("%d", &a);
+int b;
+printf("write 2nd number:");
+scanf("%d", &b);
+int c;
+printf("write 3rd number:");
+scanf("%d", &c);
+int d = a + b + c;
+printf("the average is : %d", d/3);
+
+
+int 8
+
+
+
+return 0;
+}
