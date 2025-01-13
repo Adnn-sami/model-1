8b70e6c47f434c46aa604ad97cb54a0f5333c71a
Adnn-sami <adnnsami857@gmail.com>
2025-01-13 23:40:26 +0600

    trying new

--- practise.c
+++ practise.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
+#include <iostream>
+
+int triple(int i) {
+
+  i = i * 3;
+  
+  return i;
+
+}
 
 int main() {
-    // Write code here
-    int n1 = 8;
-    int n2 = 9;
-    int n3= n1 == n2;
-    // Don't change below this line
-    printf("n1 = %d, n2 = %d, n3 = %d", n1, n2, n3);
-    return 0;
-};
+  
+  int num = 1;
+  
+  std::cout << triple(num) << "\n";
+  std::cout << triple(num) << "\n";
+
+}
