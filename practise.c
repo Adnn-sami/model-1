dd396b39185a88595a51a74cc4f7283959068214
Adnn-sami <adnnsami857@gmail.com>
2025-01-14 00:25:30 +0600

    fixed task in CA

--- practise.c
+++ practise.c
@@ -1,18 +1,18 @@
-#include <iostream>
-
-int triple(int i) {
-
-  i = i * 3;
-  
-  return i;
-
-}
+#include <stdio.h>
 
 int main() {
   
-  int num = 1;
+  // Fix the missing data types
+  int studentRank;
+  float studentFeeTotal;
+  char studentGradeLetter;
   
-  std::cout << triple(num) << "\n";
-  std::cout << triple(num) << "\n";
+  // No need to make any changes below here, use these as a hint to how each variable above should be declared
+ studentRank = 1;
+ studentFeeTotal = 100.56;
+ studentGradeLetter = 'A';
 
+ printf("Student's Rank in class: %dst\n", studentRank);
+ printf("Student's Total Fees: $%3.2f\n", studentFeeTotal);
+ printf("Student's Grade: %c\n", studentGradeLetter);
 }
